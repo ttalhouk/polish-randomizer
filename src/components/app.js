2d62614a3f5646c69fd94615a07b95df591e4bc6
Talal Talhouk <talal@sbcglobal.net>
2016-11-29 23:42:35 -0800

    update index

--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>Laura's nail polish collection</div>
+      <div>Laura's nail polish</div>
     );
   }
 }
